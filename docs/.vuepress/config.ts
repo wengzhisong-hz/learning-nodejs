1e368f61a35e5a93d7b6d2415ca6d9fbd3179b1b
wengzhisong <1322457349@qq.com>
2021-10-10 22:21:13 +0800

    feat: new sidebar

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -6,11 +6,64 @@ export default defineUserConfig<DefaultThemeOptions>({
   title: "Nodejs 技术栈",
   port: 9001,
   themeConfig: {
+    contributors: true,
+    lastUpdated: true,
     navbar: [
       {
         text: "源码解读",
-        children: ["/sourceCode/buffer.md"],
+        children: [
+          {
+            text: "Buffer",
+            link: "/sourceCode/buffer/introduction.md",
+          },
+          {
+            text: "Event",
+            link: "/sourceCode/event/introduction.md",
+          },
+        ],
+      },
+      {
+        text: "框架",
+        children: [
+          {
+            text: "Koa",
+            link: "/frame/koa/introduction.md",
+          },
+        ],
       },
     ],
+    sidebar: {
+      "/sourceCode/": [
+        {
+          text: "Buffer",
+          children: [
+            "/sourceCode/buffer/introduction.md",
+            "/sourceCode/buffer/api.md",
+            "/sourceCode/buffer/key.md",
+            "/sourceCode/buffer/practice.md",
+          ],
+        },
+        {
+          text: "Event",
+          children: [
+            "/sourceCode/event/introduction.md",
+            "/sourceCode/event/api.md",
+            "/sourceCode/event/key.md",
+            "/sourceCode/event/practice.md",
+          ],
+        },
+      ],
+      "/frame/": [
+        {
+          text: "Koa",
+          children: [
+            "/frame/koa/introduction.md",
+            "/frame/koa/api.md",
+            "/frame/koa/key.md",
+            "/frame/koa/practice.md",
+          ],
+        },
+      ],
+    },
   },
 });
---
+++ docs/sourceCode/buffer/api.md
@@ -0,0 +1,7 @@
+# API
+
+## 创建 Buffer
+
+## Buffer 字符编码
+
+## 字符串与 Buffer 类型互转
